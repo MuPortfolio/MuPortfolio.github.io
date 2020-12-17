32d70e7404e59ed1d7ee39ef7fe0946224c609ec
MuPortfolio <JustX9Plz@gmail.com>
2020-12-17 10:33:17 +0200

    Portfolio

---
+++ fanikauppa/precache-manifest.a10e677ae57bea426cd289e6481dcdc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326cde4f4ce1c59df606841dd17e6215",
+    "url": "/fanikauppa/index.html"
+  },
+  {
+    "revision": "78b76c3357f91554bd19",
+    "url": "/fanikauppa/static/css/main.4b2e18e0.chunk.css"
+  },
+  {
+    "revision": "910713dad3a66096b9cd",
+    "url": "/fanikauppa/static/js/2.49f17b50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fanikauppa/static/js/2.49f17b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78b76c3357f91554bd19",
+    "url": "/fanikauppa/static/js/main.9c4e4f6d.chunk.js"
+  },
+  {
+    "revision": "8b9bc96a5a9f75a05484",
+    "url": "/fanikauppa/static/js/runtime-main.845ccbcf.js"
+  }
+]);
